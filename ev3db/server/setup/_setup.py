15fe66d0912c29d0d1a4a3f85111998f31422f67
RikyIsola <riky.isola@gmail.com>
2020-10-04 14:26:36 +0200

    fix setup

--- ev3db/server/setup/_setup.py
+++ ev3db/server/setup/_setup.py
@@ -3,12 +3,11 @@ from os import system
 
 def setup():
     if local:
-        commands=["sshpass -p 'maker' ssh robot@ev3dev.local '"
-                  "git clone https://github.com/RikyIsola/ev3db.git tmp && "
-                  "mv tmp/ev3db . && "
-                  "mv tmp/ev3db-cli . && "
-                  "rm -rf tmp && "
-                  "echo \"maker\" | sudo -S ./ev3db-cli --setup'"]
+        commands=["git clone https://github.com/RikyIsola/ev3db.git .tmp",
+                  "sshpass -p 'maker' scp .tmp/ev3db robot@ev3dev.local:~",
+                  "sshpass -p 'maker' scp .tmp/ev3db-cli robot@ev3dev.local:~",
+                  "sshpass -p 'maker' ssh robot@ev3dev.local 'echo \"maker\" | sudo -S ./ev3db-cli --setup'",
+                  "rm -rf .tmp"]
     else:
         systemd='[Unit]\n' \
                 'Description=EV3 debug bridge service\n' \
