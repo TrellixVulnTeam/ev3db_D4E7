282d857b6dc9eb0cc44f2f29885789ac37a0949e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 07:02:23 +0000

    Adding tarfile member sanitization to extractall()

--- ev3db/server/_server.py
+++ ev3db/server/_server.py
@@ -94,7 +94,29 @@ def install(name)-> Return:
         raise HttpCode(404,'File {} not found'.format(file))
     package=file.split('-')[0]
     with tarfile.open(file,'r') as f:
-        f.extractall()
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(f)
     remove(file)
     return package
 
